e9b8073f62e1196aa57a3108c50b4aa764562027
David <davidlozano05@gmail.com>
2016-04-26 17:40:31 -0500

    Broker embebido

--- src/main/java/jms/example/main/Main.java
+++ src/main/java/jms/example/main/Main.java
@@ -6,27 +6,39 @@ import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import jms.example.broker.MessageBroker;
 import jms.example.publisher.JmsMessageSender;
- 
- 
+
 public class Main {
- 
-  public static void main(String[] args) {
-    // init spring context
-    ApplicationContext ctx = new ClassPathXmlApplicationContext("app-context.xml");
-         
-    // get bean from context
-    JmsMessageSender jmsMessageSender = (JmsMessageSender) ctx.getBean("jmsMessageSender");
-         
-    // send to default destination 
-    jmsMessageSender.send("hello JMS");
-         
-    // send to a code specified destination
-    Queue queue = new ActiveMQQueue("AnotherDest");
-    jmsMessageSender.send(queue, "hello Another Message");
-   
-    // close spring application context
-    ((ClassPathXmlApplicationContext) ctx).close();
-  }
- 
+
+	public static void main(String[] args) throws Exception {
+		
+		// init spring context
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("app-context.xml");
+
+		MessageBroker broker = new MessageBroker();
+		broker.start();
+		
+		// get bean from context
+		JmsMessageSender jmsMessageSender = (JmsMessageSender) ctx.getBean("jmsMessageSender");
+
+		// send to default destination
+		jmsMessageSender.send("Message Payload");
+
+		// send to a code specified destination
+		Queue queue = new ActiveMQQueue("AnotherDest");
+		jmsMessageSender.send(queue, "Another Message Payload");
+		
+		wait(5);
+		
+		// close spring application context
+		((ClassPathXmlApplicationContext) ctx).close();
+		
+		broker.stop();
+	}
+	
+	private static void wait(int secs) throws InterruptedException {
+		Thread.sleep(secs * 1000);
+	}
+
 }
